acb56033a6fb18b05204cd417733a9c9db50fde0
Jack <jackvarney@live.com>
2019-06-18 12:33:34 +0100

    removed dist folder...

--- dist/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var _app = require("./app");
-
-const PORT = process && process.env && process.env.PORT || undefined || 3000;
-
-_app.app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}`);
-});
